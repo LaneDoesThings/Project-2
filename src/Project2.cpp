a1a344cc73541f3aee5882041175863698c71e31
LaneDoesThings <ltw0014@uah.edu>
2024-09-28 11:55:04 -0500

    make setup

---
+++ src/Project2.cpp
@@ -0,0 +1,22 @@
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <iostream>
+#include <cmath>
+#include "OpenGL445Setup-aug24.h"
+
+#define canvas_Width 600
+#define canvas_Height 600
+#define framerate 60.0
+#define z_Plane -1 // Specified in assignment documentation
+#define box_Center 310
+#define bottom_Box_Size 180
+#define middle_Box_Size 120
+#define top_Box_Size 60
+#define snowflake_Size 50
+
+int main(int argc, char *argv[])
+{
+    printf("Hello World\n");
+    return 0;
+}
